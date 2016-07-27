4dc7dedfad4e8d48c08c4a52c3cc5ac397ac2d76
Yetty Onaeko <yetty2010@hotmail.com>
2016-07-27 23:48:08 +0100

    first commit

---
+++ Project1/TkMaxWebTest/LoginTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Project1.TkMaxWebTest
+{
+    [TestClass]
+  public class LoginTest
+    {
+        private IWebDriver _driver ;
+        [TestInitialize]
+        public void Precondition()
+        {
+         _driver = new ChromeDriver();
+            _driver .Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl("http://www.tkmaxx.com/");
+            IList<Cookie> cookies = _driver.Manage().Cookies.AllCookies;
+            foreach (var cookie in cookies)
+            {
+                Console.WriteLine(cookie);  
+            }
+            _driver.Manage().Cookies.DeleteAllCookies();
+        }
+
+        [TestCleanup]
+        public void Postcondtition()
+        {
+         //  _driver.Quit();
+        }
+
+
+
+
+        [TestMethod,TestCategory("SmokeTest")]
+        public void TestForLogin()
+        {
+            string pageSource = _driver.PageSource;
+            Console.WriteLine(pageSource);
+            bool ans = _driver.PageSource.ToLower().Contains("findstore".ToLower());
+            Assert.IsTrue(ans);
+        }
+
+
+
+    }
+}
---
+++ Project1/TkMaxWebTest/TkmaxHome Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace Project1.TkMaxWebTest
+{
+    [TestClass]
+  public class TkMaxuserTest
+    {
+        private IWebDriver _driver;
+
+       // [TestMethod, TestCategory("HomeTest")]
+       // public void checkHomePageIsLoaded()
+       // {
+         //   string pageSource = _driver.PageSource; 
+              //  bool ans = _driver.PageSource.ToLower().Contains("findstore".ToLower());
+           // Assert.IsTrue(ans);//
+       // }
+
+        [TestMethod,TestCategory("UAT")]
+        public void SignIn()
+        {
+            
+        }
+
+
+        [TestInitialize]
+        public void Precondition()
+        {
+            Console.WriteLine("This is pre condition ");
+
+        }
+
+        [TestCleanup]
+        public void Postcondtition()
+        {
+            Console.WriteLine("This run after the precondition");
+        }
+
+
+    }
+
+
+
+}
